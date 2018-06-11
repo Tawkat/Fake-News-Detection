74ca00813eef46db12bdcc7c7799827494abaa9e
Md. Tawkat Islam Khondaker <tawkat97@gmail.com>
2018-06-11 15:13:13 +0600

    Update Model.py

--- Codes/Neural Network/1. Liar Liar Pants on Fire/Model.py
+++ Codes/Neural Network/1. Liar Liar Pants on Fire/Model.py
@@ -17,27 +17,26 @@ total_word=10000
 
 time_step=300
 
+# Reading Dataset
 dataset=pd.read_csv('train.csv')
 statement=dataset.iloc[:,2:3].values
-#statement=statement.lower()
 texts=[]
 texts=dataset['Statement'].astype(str).values.tolist()
-#texts=texts.tolist()
-#statement=np.array(statement,dtype='str')
 label=dataset['Label'].astype(int).values.tolist()
 y_train=label
 y_train=np.reshape(y_train,(-1,1))
 
+#Tokenizing texts
 tokenizer_train=Tokenizer(num_words=total_word)
 tokenizer_train.fit_on_texts(texts)
 encoded_train=tokenizer_train.texts_to_sequences(texts=texts)
-#print(encoded_docs)
+
 vocab_size_train = len(tokenizer_train.word_index) + 1
 print(vocab_size_train)
 
 X_train = sequence.pad_sequences(encoded_train, maxlen=time_step,padding='post')
 
-
+#Reading Glove
 f = open('glove.6B.300d.txt',encoding='utf-8')
 embeddings_train={}
 for line in f:
@@ -65,13 +64,13 @@ print(vocab_size_train)
 print(len(embedding_matrix_train))
 
 
-# Convolution
+# Convolution Param
 filter_length = 3
 nb_filter = 128
 pool_length = 2
 
 
-
+#Creating Model
 model = Sequential()
 model.add(Embedding(vocab_size_train, embedding_size,
                     weights=[embedding_matrix_train],trainable=False))
@@ -82,11 +81,7 @@ model.add(Conv1D(filters=nb_filter,
                         activation='relu'))
 model.add(MaxPooling1D(pool_size=pool_length))
 
-
-
-
 model.add(Flatten())
-
 
 model.add(Dense(1,activation='sigmoid'))
 
@@ -97,6 +92,7 @@ model.compile(loss='binary_crossentropy',
 model.fit(X_train,y_train,batch_size=64,epochs=30)
 
 
+#Pickling to save
 with open('cnn1_llpf.pickle','wb') as f:
     pickle.dump(model, f)
 
